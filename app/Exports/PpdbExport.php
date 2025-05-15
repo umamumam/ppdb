e225744594289e1f120a8c39277788f9484ad047
umamumam <miftahulumam862@gmail.com>
2025-05-15 20:25:06 +0700

    Benerin Tgl Format Tgl Lahir

--- app/Exports/PpdbExport.php
+++ app/Exports/PpdbExport.php
@@ -3,10 +3,12 @@
 namespace App\Exports;
 
 use App\Models\Ppdb;
+use Carbon\Carbon;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class PpdbExport implements FromCollection, WithHeadings
+class PpdbExport implements FromCollection, WithHeadings, WithMapping
 {
     protected $selectedIds;
 
@@ -88,6 +90,44 @@ class PpdbExport implements FromCollection, WithHeadings
             'No KKS',
             'No PKH',
             'Jenis Pendaftar'
+        ];
+    }
+
+    public function map($row): array
+    {
+        return [
+            $row->no_pendaftaran,
+            $row->nis,
+            $row->nisn,
+            $row->nik_siswa,
+            $row->nama_siswa,
+            $row->jeniskelamin,
+            $row->tempat_lahir,
+            $row->tgl_lahir ? Carbon::parse($row->tgl_lahir)->format('d-m-Y') : '',
+            $row->kelas,
+            $row->program,
+            $row->anak_ke,
+            $row->no_kk,
+            $row->nik_ayah,
+            $row->nama_ayah,
+            $row->pendidikan_ayah,
+            $row->pekerjaan_ayah,
+            $row->nik_ibu,
+            $row->nama_ibu,
+            $row->pendidikan_ibu,
+            $row->pekerjaan_ibu,
+            $row->hp_siswa,
+            $row->hp_ortu,
+            $row->alamat,
+            $row->kode_pos,
+            $row->asal_sekolah,
+            $row->npsn,
+            $row->nsm,
+            $row->alamat_sekolah,
+            $row->no_kip,
+            $row->no_kks,
+            $row->no_pkh,
+            $row->jenis_pendaftar
         ];
     }
 }
