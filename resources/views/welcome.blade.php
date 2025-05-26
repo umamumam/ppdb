30c1297298bf0179c5fcc9b2670ececd7927fe0d
umamumam <miftahulumam862@gmail.com>
2025-05-26 20:55:53 +0700

    UP

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -77,7 +77,7 @@
                         <ul class="nav">
                             <li class="scroll-to-section"><a href="/" class="{{ Request::is('/') ? 'active' : '' }}">Home</a></li>
                             <li class="scroll-to-section"><a href="/ppdb/create" class="{{ Request::is('ppdb/create') ? 'active' : '' }}">PPDB</a></li>
-                            <li class="scroll-to-section"><a href="/login" class="{{ Request::is('login') ? 'active' : '' }}">Login</a></li>
+                            <li class="scroll-to-section"><a href="/login" class="{{ Request::is('login') ? 'active' : '' }}" style="color: red">Login</a></li>
                             {{-- <li class="scroll-to-section"><a href="/visi" class="{{ Request::is('visi') ? 'active' : '' }}">Tentang</a></li> --}}
                             {{-- <li class="scroll-to-section"><a href="/ppdb/search" class="{{ Request::is('ppdb/search') ? 'active' : '' }}">Bukti Pendaftaran</a></li> --}}
                             {{-- <li class="scroll-to-section"><a href="#contact">Program Unggulan</a></li>
