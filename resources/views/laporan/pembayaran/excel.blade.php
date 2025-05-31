c2457ed0f23e71d86573a143f30e36952e3251c4
umamumam <miftahulumam862@gmail.com>
2025-05-31 08:21:11 +0700

    Penyesuaian Format Excel

--- resources/views/laporan/pembayaran/excel.blade.php
+++ resources/views/laporan/pembayaran/excel.blade.php
@@ -1,19 +1,43 @@
 {{-- resources/views/laporan/pembayaran/excel.blade.php --}}
 <!DOCTYPE html>
 <html>
+
 <head>
     <style>
-        table, th, td { border: 1px solid black; border-collapse: collapse; }
-        th, td { padding: 5px; }
-        th { background-color: #f2f2f2; }
-        .text-right { text-align: right; }
-        .text-center { text-align: center; }
-        .total-row { font-weight: bold; }
+        table,
+        th,
+        td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+
+        th,
+        td {
+            padding: 5px;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+
+        .text-right {
+            text-align: right;
+        }
+
+        .text-center {
+            text-align: center;
+        }
+
+        .total-row {
+            font-weight: bold;
+        }
     </style>
 </head>
+
 <body>
     <h2 style="text-align: center;">LAPORAN PEMBAYARAN MA DARUL FALAH</h2>
-    <p style="text-align: center;">Periode: {{ \Carbon\Carbon::parse($startDate)->format('d/m/Y') }} - {{ \Carbon\Carbon::parse($endDate)->format('d/m/Y') }}</p>
+    <p style="text-align: center;">Periode: {{ \Carbon\Carbon::parse($startDate)->format('d/m/Y') }} - {{
+        \Carbon\Carbon::parse($endDate)->format('d/m/Y') }}</p>
 
     <table>
         <thead>
@@ -34,36 +58,36 @@
         </thead>
         <tbody>
             @foreach($pembayarans as $key => $pembayaran)
-                @php
-                    $total = $pembayaran->nominal_spp + $pembayaran->nominal_infaq +
-                             $pembayaran->nominal_seragam + $pembayaran->nominal_kitab +
-                             $pembayaran->nominal_kolektif;
-                @endphp
-                <tr>
-                    <td class="text-center">{{ $key + 1 }}</td>
-                    <td>{{ $pembayaran->tgl_bayar->format('d/m/Y') }}</td>
-                    <td>{{ $pembayaran->ppdb->nama_siswa }}</td>
-                    <td>{{ $pembayaran->jenis_pembayaran }}</td>
-                    <td class="text-right">{{ number_format($pembayaran->nominal_spp, 0, ',', '.') }}</td>
-                    <td class="text-right">{{ number_format($pembayaran->nominal_infaq, 0, ',', '.') }}</td>
-                    <td class="text-right">{{ number_format($pembayaran->nominal_seragam, 0, ',', '.') }}</td>
-                    <td class="text-right">{{ number_format($pembayaran->nominal_kitab, 0, ',', '.') }}</td>
-                    <td class="text-right">{{ number_format($pembayaran->nominal_kolektif, 0, ',', '.') }}</td>
-                    <td class="text-right">{{ number_format($total, 0, ',', '.') }}</td>
-                    <td class="text-center">{{ $pembayaran->status }}</td>
-                    <td>{{ $pembayaran->petugas->nama ?? '-' }}</td>
-                </tr>
+            @php
+            $total = $pembayaran->nominal_spp + $pembayaran->nominal_infaq +
+            $pembayaran->nominal_seragam + $pembayaran->nominal_kitab +
+            $pembayaran->nominal_kolektif;
+            @endphp
+            <tr>
+                <td class="text-center">{{ $key + 1 }}</td>
+                <td>{{ $pembayaran->tgl_bayar->format('d/m/Y') }}</td>
+                <td>{{ $pembayaran->ppdb->nama_siswa }}</td>
+                <td>{{ $pembayaran->jenis_pembayaran }}</td>
+                <td class="text-right">{{ $pembayaran->nominal_spp }}</td>
+                <td class="text-right">{{ $pembayaran->nominal_infaq }}</td>
+                <td class="text-right">{{ $pembayaran->nominal_seragam }}</td>
+                <td class="text-right">{{ $pembayaran->nominal_kitab }}</td>
+                <td class="text-right">{{ $pembayaran->nominal_kolektif }}</td>
+                <td class="text-right">{{ $total }}</td>
+                <td class="text-center">{{ $pembayaran->status }}</td>
+                <td>{{ $pembayaran->petugas->nama ?? '-' }}</td>
+            </tr>
             @endforeach
         </tbody>
         <tfoot>
             <tr class="total-row">
                 <td colspan="4">TOTAL</td>
-                <td class="text-right">{{ number_format($totalSpp, 0, ',', '.') }}</td>
-                <td class="text-right">{{ number_format($totalInfaq, 0, ',', '.') }}</td>
-                <td class="text-right">{{ number_format($totalSeragam, 0, ',', '.') }}</td>
-                <td class="text-right">{{ number_format($totalKitab, 0, ',', '.') }}</td>
-                <td class="text-right">{{ number_format($totalKolektif, 0, ',', '.') }}</td>
-                <td class="text-right">{{ number_format($totalAll, 0, ',', '.') }}</td>
+                <td class="text-right">{{ $totalSpp }}</td>
+                <td class="text-right">{{ $totalInfaq }}</td>
+                <td class="text-right">{{ $totalSeragam }}</td>
+                <td class="text-right">{{ $totalKitab }}</td>
+                <td class="text-right">{{ $totalKolektif }}</td>
+                <td class="text-right">{{ $totalAll }}</td>
                 <td colspan="2"></td>
             </tr>
         </tfoot>
@@ -71,4 +95,5 @@
 
     <p style="text-align: right;">Dicetak pada: {{ now()->format('d/m/Y H:i:s') }}</p>
 </body>
+
 </html>
